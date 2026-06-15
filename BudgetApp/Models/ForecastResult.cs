f0c6793f4454248049e5ddb874894b0472a251d2
Bbalchik <nazarpalnnij7@gmail.com>
2026-06-15 18:29:22 +0300

    Edit encapsulation

--- BudgetApp/Models/ForecastResult.cs
+++ BudgetApp/Models/ForecastResult.cs
@@ -2,9 +2,18 @@ namespace BudgetApp.Models;
 
 public class ForecastResult
 {
-    public decimal ProjectedMonthlyExpenses { get; set; }
-    public decimal ProjectedMonthlyIncome { get; set; }
+    public decimal ProjectedMonthlyExpenses { get; private set; }
+    public decimal ProjectedMonthlyIncome { get; private set; }
+    public decimal DailyAverageExpense { get; private set; }
+    public decimal DailyAverageIncome { get; private set; }
+
     public decimal ProjectedBalance => ProjectedMonthlyIncome - ProjectedMonthlyExpenses;
-    public decimal DailyAverageExpense { get; set; }
-    public decimal DailyAverageIncome { get; set; }
+
+    public ForecastResult(decimal projectedMonthlyExpenses, decimal projectedMonthlyIncome, decimal dailyAverageExpense, decimal dailyAverageIncome)
+    {
+        ProjectedMonthlyExpenses = projectedMonthlyExpenses >= 0 ? projectedMonthlyExpenses : 0;
+        ProjectedMonthlyIncome = projectedMonthlyIncome >= 0 ? projectedMonthlyIncome : 0;
+        DailyAverageExpense = dailyAverageExpense >= 0 ? dailyAverageExpense : 0;
+        DailyAverageIncome = dailyAverageIncome >= 0 ? dailyAverageIncome : 0;
+    }
 }
